8b1449f48054355813ea252299a3f608315195c4
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-23 12:51:50 -0400

    Change test to test netflix.seewilsoncode.com and test for searching for 'harry potter'

--- test/test.js
+++ test/test.js
@@ -3,20 +3,17 @@ const expect = require('chai').expect;
 describe('My React Test', function () {
   beforeEach(function() {
     browser.url('./');
-    browser.waitForText('[data-reactroot]');
   })
 
-  it('should find moana', function () {
-    browser.setValue('#q', 'moana');
+  it('should click search', function () {
+    browser.click('.searchTab');
 
-    browser.waitForText('.tt-dropdown-menu');
-
-    browser.keys('Down arrow');
-    browser.keys('Enter');
+    browser.waitForExist('.searchBox');
+    browser.addValue('input', 'harry potter')
+    browser.keys('Enter')
 
     let pageHeading = browser.getText('h1');
 
-    expect(pageHeading).to.equal('MOANA');
-    expect(browser.getText('.tagline')).to.equal('The ocean is calling.');
+    expect(pageHeading).to.equal('Found 12 results for Harry Potter');
   })
 })
--- wdio.conf.js
+++ wdio.conf.js
@@ -72,7 +72,7 @@ exports.config = {
     //
     // Set a base URL in order to shorten url command calls. If your url parameter starts
     // with "/", then the base url gets prepended.
-    baseUrl: 'https://skempin.github.io/reactjs-tmdb-app/',
+    baseUrl: 'http://netflix.seewilsoncode.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
